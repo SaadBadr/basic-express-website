8123f8528a824b43bbb0b41b7c750533b99564dd
SaadBadr <saadbadr23@gmail.com>
2021-04-26 14:21:25 +0200

    server rendering with pug templates adding homepage and about page

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const nodemailer = require('nodemailer')
 const port = 3000
 const app  = express()
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {title: "About us!"})
 })
 
 app.listen(port)
